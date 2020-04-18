433d3b33dc4888f3bc8952c718c2ca58d7cd383b
toddchrun <toddchrun@gmail.com>
2020-04-18 09:28:22 -0500

    expanding card and deck objects

--- deck.py
+++ deck.py
@@ -2,15 +2,20 @@
 # Class for each card
 #
 ###############################################################################
+from card import Card
 
 class Deck():
     """
     Class for every deck
     """
 
-    def __init__(self, card):
+    def __init__(self):
         super(Deck, self).__init__()
+        suit_index = ["c", "s", "d", "h"]
+        value_index = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
         full_deck = []
 
-        for
+        for s_index, v_index in suit_index, value_index:
+            full_deck.insert(Card{v_index, s_index})
+            
